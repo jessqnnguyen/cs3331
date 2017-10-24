bcf3558f0d6835c8ee707a6ee9ab0acc9eb7be1d
Jess <jessqnnguyen@gmail.com>
2017-10-25 00:38:05 +1100

    fixed a few bugs with packet numbers and routing problems in assignment 2

--- assignments/ass02/RoutingPerformance.py
+++ assignments/ass02/RoutingPerformance.py
@@ -7,11 +7,13 @@ import queue
 import statistics
 
 def main(argv):
-    # network_scheme = argv[1] # "CIRCUIT" or "PACKET"
-    routing_scheme = argv[1] # "SHP" "SDP" or "LLP"
-    topology_file = argv[2]
-    workload_file = argv[3]
-    packet_rate = int(argv[4]) # positive num, packets / sec
+    network_scheme = argv[1] # "CIRCUIT" or "PACKET"
+    routing_scheme = argv[2] # "SHP" "SDP" or "LLP"
+    topology_file = argv[3]
+    workload_file = argv[4]
+    packet_rate = int(argv[5]) # positive num, packets / sec
+
+    print("using a %s network scheme!" % (network_scheme))
 
     num_vc_requests = 0
     num_total_packets = 0
@@ -62,81 +64,137 @@ def main(argv):
         ttl = line_args[3].rstrip()
         # print(str(n) + " " + str(time) + " " + src + " " + dest + " " + str(ttl))
         add_request(requests, time, src, dest, ttl)
-        start_time = round(float(time), 1)
+        # start_time = round(float(time), 1)
+        start_time = float(time)
         request_timestamps.append(start_time)
-        end_time = round(float(time) + float(ttl), 1)
+        end_time = float(time) + float(ttl)
+        # end_time = round(float(time) + float(ttl), 1)
         request_timestamps.append(end_time)
         line = f.readline()
     f.close()
 
-    print("request_timestamps size =  " + str(len(request_timestamps)))
+    # print("request_timestamps size =  " + str(len(request_timestamps)))
     # for k,v in sorted(requests.items()):
     #     print(k, v)
 
-    request_timestamps.sort()
-    print(request_timestamps)
-    curr_request_num = 0
-    next_request_time = request_timestamps[curr_request_num]
-    t = next_request_time
+    # request_timestamps.sort()
+    # print(request_timestamps)
+    # curr_request_num = 0
+    # next_request_time = request_timestamps[curr_request_num]
+    # t = next_request_time
 
     jobs = queue.PriorityQueue()
 
     print("requests size = " + str(requests.items()))
 
-    for k, v in sorted(requests.items()):
-        for r in v:
-            print("r : " + str(r))
-            (src, dest, ttl) = r
-            addr = (src, dest)
-            path = find_shortest_path(g, e, src, dest, routing_scheme)
-            jobs.put((k, addr, path, "allocate"))
-            deallocate_timestamp = k + ttl
-            jobs.put((deallocate_timestamp, addr, path, "deallocate"))
-
+    job_id = 0
+    if network_scheme.upper() == "CIRCUIT":
+        for k, v in sorted(requests.items()):
+            for r in v:
+                print("r : " + str(r))
+                (src, dest, ttl) = r
+                packets_to_send = math.floor(float(packet_rate * ttl))
+                num_total_packets += packets_to_send
+                addr = (src, dest)
+                jobs.put((k, job_id, addr, "allocate"))
+                job_id += 1
+                deallocate_timestamp = k + ttl
+                jobs.put((deallocate_timestamp, job_id, addr, "deallocate"))
+                job_id += 1
+    elif network_scheme.upper() == "PACKET":
+        print("................................................................................")
+        print("computing vcs for a packet network")
+        virtual_circuits = queue.PriorityQueue()
+        job_id = 0
+        for k, v in sorted(requests.items()):
+            for r in v:
+                print("r : " + str(r))
+                (src, dest, ttl) = r
+                addr = (src, dest)
+                num_packets_to_send = math.floor(float(packet_rate * ttl))
+                print("packets to send = %d" % num_packets_to_send)
+                second_increment = 1 / packet_rate
+                start_time = k
+                for packet_id in range(0, num_packets_to_send):
+                    end_time = start_time + second_increment
+                    num_total_packets += 1
+                    virtual_circuits.put((start_time, job_id, packet_id, addr, "allocate"))
+                    jobs.put((start_time, job_id, packet_id, addr, "allocate"))
+                    virtual_circuits.put((end_time, job_id, packet_id, addr, "deallocate"))
+                    jobs.put((end_time, job_id, packet_id, addr, "deallocate"))
+                    start_time = end_time
+                job_id += 1
+        print("................................................................................")
+        print("printed computed vcs for a packet network")
+        while not(virtual_circuits.empty()):
+            vc = virtual_circuits.get()
+            print(vc)
     print("queue size: " + str(jobs.qsize()))
+
+    job_id_paths = dict()
+    job_ids_blocked = dict()
     # while not(jobs.empty()):
     #     job = jobs.get()
     #     print(job)
     # Add jobs to the queue from the requests
     while not(jobs.empty()):
-        print("t =  " + str(t))
-        print("next request time = " + str(next_request_time))
-        print ("curr_request_num =  " + str(curr_request_num))
-        if t == next_request_time:
-            for i in range(curr_request_num, len(request_timestamps)):
-                timestamp = request_timestamps[i]
-                print("curr timestamp checking =  " + str(timestamp))
-                if timestamp == t:
-                    print ("next request time event registered")
-                    (timestamp, addr, path, job_type) = jobs.get()
-                    print("queue size now = " + str(jobs.qsize()))
-                    print("current job processsing %s %s" % (timestamp, job_type))
-                    curr_request_num += 1
-                    if curr_request_num < len(request_timestamps):
-                        next_request_time = request_timestamps[curr_request_num]
-                    src = addr[0]
-                    dest = addr[1]
-                    print ("addr: " + str(addr))
-                    # ttl = float(v[2])
-                    if job_type == "allocate":
-                        print("allocating")
-                        print("e: " + str(e))
-                        print("checking capacity... " + str(check_capacity(e, path)))
-                        packets_to_send = math.floor(float(packet_rate * ttl))
-                        num_total_packets += packets_to_send
-                        if check_capacity(e, path):
-                            establish_circuit(e, path)
-                            num_successfully_routed_packets += packets_to_send
-                            hops.append(len(path))
-                            path_delay = compute_prop_delay(e, path)
-                            prop_delays.append(path_delay)
-                        else:
-                            print("blocked")
-                            num_blocked_packets += packets_to_send
-                    elif job_type == "deallocate":
-                        print("deallocating")
-                        terminate_circuit(e, path)
-        t = round((t + 0.1), 1)
+        # print("t =  " + str(t))
+        # print("next request time = " + str(next_request_time))
+        # print ("curr_request_num =  " + str(curr_request_num))
+        # if t == next_request_time:
+            # for i in range(curr_request_num, len(request_timestamps)):
+                # timestamp = request_timestamps[i]
+                # print("curr timestamp checking =  " + str(timestamp))
+                # if timestamp == t:
+                    # print ("next request time event registered")
+        if network_scheme.upper() == "CIRCUIT":
+            (timestamp, job_id, addr, job_type) = jobs.get()
+        else:
+            (timestamp, job_id, packet_id, addr, job_type) = jobs.get()
+        print("queue size now = " + str(jobs.qsize()))
+        print("current job processsing %s %s" % (timestamp, job_type))
+        # curr_request_num += 1
+        # if curr_request_num < len(request_timestamps):
+            # next_request_time = request_timestamps[curr_request_num]
+        src = addr[0]
+        dest = addr[1]
+        print ("addr: " + str(addr))
+        path = []
+        if network_scheme.upper() == "PACKET":
+            temp = str(job_id) + str(packet_id)
+            job_id = temp
+        if not(job_id in job_id_paths.keys()):
+            print("***********************************************************************************")
+            print("path has not yet been calculated for job! (mostly likely because it's an alloc job)")
+            print("calculating allocate path for %s -> %s with job id %s" % (src, dest, job_id))
+            path = find_shortest_path(g, e, src, dest, routing_scheme)
+            job_id_paths[job_id] = path
+            print("path found : " + str(path))
+        else:
+            path = job_id_paths[job_id]
+        # ttl = float(v[2])
+        if job_type == "allocate":
+            print("allocating")
+            print("e: " + str(e))
+            print("checking capacity... " + str(check_capacity(e, path)))
+            if check_capacity(e, path):
+                establish_circuit(e, path)
+                num_successfully_routed_packets += packets_to_send
+                hops.append(len(path))
+                path_delay = compute_prop_delay(e, path)
+                prop_delays.append(path_delay)
+            else:
+                job_ids_blocked[job_id] = True
+                print("blocked packet with job id %s" % (job_id))
+                num_blocked_packets += packets_to_send
+                print("num blocked packets = %d" % num_blocked_packets)
+        elif job_type == "deallocate":
+            if not(job_id in job_ids_blocked):
+                print("deallocating job id %s" % job_id)
+                print("***********************************************************************************")
+                terminate_circuit(e, path)
+                print("e: " + str(e))
+        # t = round((t + 0.1), 1)
 
     # path = find_shortest_path(g, e, "A", "D")
     # print(str(path))
@@ -152,7 +210,7 @@ def main(argv):
     for hop_num in hops:
         total += hop_num
     avg_num_hops = total / len(hops)
-    avg_num_hops = statistics.mean(hops)
+    avg_num_hops = round(statistics.mean(hops), 2)
 
     cumulative_average_prop_delays = []
     i = 1
@@ -162,7 +220,7 @@ def main(argv):
         cumulative_average_prop_delays.append(curr_sum / i)
         i += 1
 
-    average_cumulative_prop_delay = statistics.mean(cumulative_average_prop_delays)
+    average_cumulative_prop_delay = round(statistics.mean(cumulative_average_prop_delays), 2)
 
     print("total number of virtual connection requests: " + str(num_vc_requests))
     print("total number of packets: " + str(num_total_packets))
@@ -173,22 +231,22 @@ def main(argv):
     print("average number of hops per circuit: " + str(avg_num_hops))
     print("average cumulative propagation delay per circuit: " + str(average_cumulative_prop_delay))
 
-def process_next_job():
-    (timestamp, v) = jobs.get()
-    src = v[0]
-    dest = v[1]
-    ttl = float(v[2])
-    packets_to_send = math.floor(float(packet_rate * ttl))
-    num_total_packets += packets_to_send
-    path = find_shortest_path(g, e, src, dest, routing_scheme)
-    if check_capacity:
-        establish_circuit(e, path)
-        num_successfully_routed_packets += packets_to_send
-        hops.append(len(path))
-        path_delay = compute_prop_delay(e, path)
-        prop_delays.append(path_delay)
-    else:
-        num_blocked_packets += packets_to_send
+# def process_next_job():
+#     (timestamp, v) = jobs.get()
+#     src = v[0]
+#     dest = v[1]
+#     ttl = float(v[2])
+#     packets_to_send = math.floor(float(packet_rate * ttl))
+#     num_total_packets += packets_to_send
+#     path = find_shortest_path(g, e, src, dest, routing_scheme)
+#     if check_capacity:
+#         establish_circuit(e, path)
+#         num_successfully_routed_packets += packets_to_send
+#         hops.append(len(path))
+#         path_delay = compute_prop_delay(e, path)
+#         prop_delays.append(path_delay)
+#     else:
+#         num_blocked_packets += packets_to_send
 
 def compute_prop_delay(e, path):
     total_delay = 0
@@ -220,16 +278,23 @@ def terminate_circuit(e, path):
         curr_link1 = path[i] + path[i + 1] # e.g. AB
         curr_link2 = path[i + 1] + path[i] # e.g. BA
         (prop_delay, max_capacity, curr_capacity) = e[curr_link1]
-        e[curr_link1] = (prop_delay, max_capacity, curr_capacity - 1)
-        e[curr_link2] = (prop_delay, max_capacity, curr_capacity - 1)
+        if curr_capacity != 0:
+            e[curr_link1] = (prop_delay, max_capacity, curr_capacity - 1)
+            e[curr_link2] = (prop_delay, max_capacity, curr_capacity - 1)
 
-def find_shortest_path(g, e, src, dest, protocol):
+def find_shortest_path(g, e, src, dest, protocol_name):
+    print("*-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-*")
+    print("commencing dijkstra woo!")
+    print("*-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-**-*-*")
+    protocol = protocol_name.upper()
     if protocol == "SHP":
         (dist, st) = dijkstra_shp(g, e, src, dest)
-    if protocol == "SDP":
+    elif protocol == "SDP":
         (dist, st) = dijkstra_sdp(g, e, src, dest)
-    if protocol == "LLP":
+    elif protocol == "LLP":
         (dist, st) = dijkstra_llp(g, e, src, dest)
+    else:
+        raise NameError("%s is not a valid routing protocol argument!" % (protocol))
     path = []
     while True:
         path.append(dest)
@@ -240,6 +305,7 @@ def find_shortest_path(g, e, src, dest, protocol):
     return path
 
 def dijkstra_llp(g, e, src, dest):
+    visited_edges = []
     dist = dict() # final distances
     dist[src] = 0
     st = dict() # previous node in optimal path from src
@@ -258,21 +324,42 @@ def dijkstra_llp(g, e, src, dest):
                 st[v] = ""
 
         q.put((dist[src], src))
-        while (not q.empty()):
+        while not(q.empty()):
+            print("this is running")
             # remove and return the best vertex
             (d, u) = q.get()
+            # visited_nodes.append(u)
+            print("q.get() : (%d, %s)" % (d, u))
             # print ("u = " + str(u))
             if u == dest:
+                print("%s is the dest!" % u)
                 break
             for v in g[u]:
                 link = u + v
+                if link in visited_edges:
+                    continue
+                print("now considering link (%s, %s)" % (u, v))
                 (d, c, cc) = e[link]
+                print("-----------------------------------------------------")
+                print("link (%s -> %s) is currently serving %d connection(s)" % (u, v, cc))
                 link_load = (cc / c)
+                print("link load = " + str(link_load))
                 alt = link_load
+                print("alt = %f dist[%s] = %f" % (alt, v, dist[v]))
                 if alt < dist[v]:
+                    print("alt < dist[%s]!" % v)
                     dist[v] = alt
+                    print("setting dist[%s] = %d" % (v, alt))
                     st[v] = u
+                    print("setting st[%s] = %s" % (v, u))
                     q.put((dist[v], v))
+                same_link = v + u
+                visited_edges.append(link)
+                visited_edges.append(same_link)
+
+    print("returning!")
+    for k, v in st.items():
+        print(k, v)
     return (dist, st)
 
 def dijkstra_sdp(g, e, src, dest):
